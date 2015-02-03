a82d16058cd8343d50f09cd02ddd8f475d6bd97a
Tamer Aydin <tamer.aydin@gmail.com>
2015-02-03 18:55:31 +0100

    implemented basic learn & generate methods

--- dist/logical-phrase.js
+++ dist/logical-phrase.js
@@ -8,19 +8,100 @@
 
   var root = this;
 
-  var LogicalPhrase = function() {
+  /**
+   *
+   * {
+   *   "items": [...],
+   *   "type": 'AND'|'OR'|'NOT'
+   * }
+   *
+   * {
+   *   "text": "...",
+   *   "type": 'AND'|'OR'|'NOT'
+   * }
+   *
+   */
 
+  // LOCALS
+  var _TYPE_MAP = {
+    'AND': 'and',
+    'OR': 'or',
+    'NOT': 'falsy'
+  };
+
+  //TODO: use call instead of passing 'lp' as argument
+  function _wrap(lp, text, level) {
+    if (lp.levelWrappers[level]) {
+      var wrapperStartTag = lp.levelWrappers[level].replace(' /', '');
+      var wrapperEndTag = wrapperStartTag.split(' ')[0];
+
+      wrapperEndTag = wrapperEndTag.replace('<', '</');
+      if (wrapperEndTag[wrapperEndTag.length - 1] !== '>') {
+        wrapperEndTag += '>';
+      }
+
+      text = wrapperStartTag + text + wrapperEndTag;
+    }
+
+    return text;
+  }
+
+
+  //TODO: optimize & simplify
+  function _getTypeText(lp, type, filter) {
+    var typeMap = _TYPE_MAP[type];
+
+    return (type && (!filter || filter.indexOf(typeMap) > -1)) ?
+      (lp[typeMap] + ' ') : (lp.truthy ? lp.truthy + ' ' : '');
+  }
+
+  function _getText(lp, data, level) {
+    level = level || 0;
+
+    var itemTextList = [];
+    var typeText;
+
+    for (var i = 0; i < data.items.length; i++) {
+      var item = data.items[i];
+      typeText = _getTypeText(lp, item.type, ['falsy']);
+
+      if (item.text) {
+        itemTextList.push(typeText + item.text + ' ');
+      }
+
+      if (item.items) {
+        var nextLevel = (item.type && _TYPE_MAP[item.type] !== 'falsy') ? level + 1 : level;
+
+        itemTextList.push(typeText + _getText(lp, item, nextLevel) + ' ');
+      }
+    }
+
+    return (lp.prefix ? lp.prefix + ' ' : '') +
+      _wrap(lp, itemTextList.join(_getTypeText(lp, data.type)).replace(/^\s+|\s+$/g, ''), level);
+  }
+
+  // MODULE
+  var LogicalPhrase = function() {
+    this.prefix = '';
+
+    this.and = 'AND';
+    this.or = 'OR';
+    this.truthy = '';
+    this.falsy = 'NOT';
+
+    this.levelWrappers = [];
   };
 
   LogicalPhrase.prototype = {
-    learn: function() {
-      console.log(1);
+    learn: function(obj) {
+      for (var key in obj) {
+        if (typeof this[key] !== 'undefined') {
+          this[key] = obj[key];
+        }
+      }
     },
-    forget: function() {
-
-    },
-    generateBy: function() {
-
+    generateBy: function(data) {
+      return _getText(this, data);
     }
   };
 
--- test/logical-phrase.spec.js
+++ test/logical-phrase.spec.js
@@ -2,9 +2,304 @@ if (typeof LogicalPhrase === 'undefined') {
   var LogicalPhrase = require('../src/main.js');
 }
 
+var LP, data;
+
+//TODO: seperate into different specs
 describe('logical-phrase', function() {
 
-  it('should initialize a constructor', function() {
-    expect(typeof LogicalPhrase).toBe('function');
+  describe('module', function() {
+
+    it('should initialize a constructor', function() {
+      expect(typeof LogicalPhrase).toBe('function');
+    });
+  });
+
+  describe('learn', function() {
+    LP = new LogicalPhrase();
+
+    it('prefix', function() {
+      LP.learn({
+        'prefix': 'x',
+        'truthy': 'did',
+        'levelWrappers': [
+          '<div>',
+          '<span>'
+        ]
+      });
+
+      expect(LP.prefix).toBe('x');
+      expect(LP.truthy).toBe('did');
+      expect(LP.levelWrappers[0]).toBe('<div>');
+      expect(LP.levelWrappers[1]).toBe('<span>');
+    });
+  });
+
+  describe('generate', function() {
+    beforeEach(function() {
+      LP = new LogicalPhrase();
+    });
+
+    it('case 1', function() {
+      data = {
+        "items": [
+          {
+            "text": "x"
+          },
+          {
+            "text": "y"
+          }
+        ],
+        "type": "AND"
+      };
+
+      expect(LP.generateBy(data)).toBe('x AND y');
+    });
+
+    it('case 2', function() {
+      data = {
+        "items": [
+          {
+            "text": "x"
+          },
+          {
+            "text": "y",
+            "type": "NOT"
+          }
+        ],
+        "type": "AND"
+      };
+
+      expect(LP.generateBy(data)).toBe('x AND NOT y');
+    });
+
+    it('case 3', function() {
+      data = {
+        "items": [
+          {
+            "items": [
+              {
+                "text": "x"
+              },
+              {
+                "text": "y"
+              }
+            ],
+            "type": "OR"
+          },
+          {
+            "text": "z",
+            "type": "NOT"
+          }
+        ],
+        "type": "AND"
+      };
+
+      expect(LP.generateBy(data)).toBe('x OR y AND NOT z');
+    });
+
+    it('case 4', function() {
+      data = {
+        "items": [
+          {
+            "items": [
+              {
+                "items": [
+                  {
+                    "text": "x"
+                  },
+                  {
+                    "text": "y"
+                  }
+                ],
+                "type": "OR"
+              }
+            ],
+            "type": "NOT"
+          },
+          {
+            "text": "z"
+          }
+        ],
+        "type": "AND"
+      };
+
+      expect(LP.generateBy(data)).toBe('NOT x OR y AND z');
+    });
+  });
+
+  describe('combined', function() {
+    var prefix = 'select ones that';
+    var truthy = 'did';
+    var falsy = 'did not';
+
+    beforeEach(function() {
+      LP = new LogicalPhrase();
+    });
+
+    //TODO: tests for each property
+
+    it('should reflect prefix/truthy/values', function() {
+      LP.learn({
+        'prefix': prefix,
+        'truthy': truthy,
+        'falsy': falsy
+      });
+
+      data = {
+        "items": [
+          {
+            "text": "x"
+          },
+          {
+            "text": "y",
+            "type": "NOT"
+          }
+        ],
+        "type": "AND"
+      };
+
+      expect(LP.generateBy(data)).toBe(
+        prefix + ' ' + truthy + ' x AND ' + falsy + ' y');
+    });
+
+    it('should wrap with certain level elements', function() {
+      LP.learn({
+        'levelWrappers': [
+          '<div class="level-0">',
+          '<span class="level-1" />',
+          '<span>'
+        ]
+      });
+
+      data = {
+        "items": [
+          {
+            "text": "x"
+          },
+          {
+            "text": "y"
+          }
+        ],
+        "type": "AND"
+      };
+
+      expect(LP.generateBy(data)).toBe(
+        '<div class="level-0">x AND y</div>');
+
+      data = {
+        "items": [
+          {
+            "items": [
+              {
+                "text": "x"
+              },
+              {
+                "text": "y"
+              }
+            ],
+            "type": "OR"
+          },
+          {
+            "text": "z",
+            "type": "NOT"
+          }
+        ],
+        "type": "AND"
+      };
+
+      expect(LP.generateBy(data)).toBe(
+        '<div class="level-0"><span class="level-1">x OR y</span> AND NOT z</div>');
+
+      LP.learn({
+        'levelWrappers': [
+          null,
+          '<b>',
+          '<span>'
+        ]
+      });
+
+      data = {
+        "items": [
+          {
+            "items": [
+              {
+                "text": "x"
+              },
+              {
+                "text": "y"
+              }
+            ],
+            "type": "OR"
+          },
+          {
+            "items": [
+              {
+                "text": "a"
+              },
+              {
+                "text": "b"
+              }
+            ],
+            "type": "AND"
+          },
+          {
+            "text": "c"
+          },
+          {
+            "items": [
+              {
+                "items": [
+                  {
+                    "text": "d"
+                  },
+                  {
+                    "text": "e"
+                  }
+                ],
+                "type": "OR"
+              },
+              {
+                "text": "f"
+              }
+            ],
+            "type": "AND"
+          },
+          {
+            "items": [
+              {
+                "items": [
+                  {
+                    "text": "z"
+                  },
+                  {
+                    "text": "w"
+                  },
+                  {
+                    "text": "q",
+                    "type": "NOT"
+                  }
+                ],
+                "type": "OR"
+              }
+            ],
+            "type": "NOT"
+          }
+        ],
+        "type": "AND"
+      };
+
+      expect(LP.generateBy(data)).toBe(
+        '<b>x OR y</b>' +
+        ' AND ' +
+        '<b>a AND b</b>' +
+        ' AND ' +
+        'c' +
+        ' AND ' +
+        '<b><span>d OR e</span> AND f</b>' +
+        ' AND NOT ' +
+        '<b>' +
+          'z OR w OR NOT q' +
+        '</b>');
+    });
   });
 });
